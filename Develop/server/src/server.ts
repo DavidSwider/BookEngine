a2e23713007505073d510109e5acc56455ebe205
DavidSwider <davidswider6@gmail.com>
2025-03-23 23:53:29 -0400

    db change

--- Develop/server/src/server.ts
+++ Develop/server/src/server.ts
@@ -43,8 +43,8 @@ if (process.env.NODE_ENV === 'production') {
 
 
 console.log("before db connection");
-// db.once('open', () => {
+db.once('open', () => {
   app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}/graphql`));
-// });
+});
 }
 startApolloServer();
