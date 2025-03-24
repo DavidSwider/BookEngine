695d9b4e6877a07141a9022593bd715a1f9a0bda
DavidSwider <davidswider6@gmail.com>
2025-03-23 23:40:15 -0400

    updated config connection and server

--- Develop/server/src/server.ts
+++ Develop/server/src/server.ts
@@ -1,4 +1,4 @@
-import express from 'express';
+import express, { Request, Response } from 'express';
 import path from 'node:path';
 import db from './config/connection.js';
 import typeDefs from './schema/typedefs.js';
@@ -10,17 +10,23 @@ import { fileURLToPath } from 'url';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
-const app = express();
-const PORT = process.env.PORT || 3001;
+
+
 const server = new ApolloServer({
   typeDefs,
   resolvers,
 });
+
 const startApolloServer=async()=>{
   await server.start();
   await db;
+  
+  const PORT = process.env.PORT || 3001;
+  const app = express();
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
 app.use("/graphql",expressMiddleware(server as any,
   {
     context: authenticateToken as any
@@ -30,6 +36,9 @@ app.use("/graphql",expressMiddleware(server as any,
 // if we're in production, serve client/build as static assets
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
+  app.get('*', (_req: Request, res: Response) => {
+    res.sendFile(path.join(__dirname, '../../client/dist/index.html'));
+  });
 }
 
 
